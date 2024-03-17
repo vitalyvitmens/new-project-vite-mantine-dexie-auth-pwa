ad586bc01f11fa32f9da301cdd78f630ddb3d456
vitalyvitmens <otm_@tut.by>
2024-03-17 17:40:07 +0300

    Updates

---
+++ assets/use-id-wrMgMW9K.js
@@ -0,0 +1,1 @@
+import{R as o,r as s,E as u,F as i}from"./index-DKGbaZLL.js";const d=o.useId||(()=>{});function a(){const e=d();return e?`mantine-${e.replace(/:/g,"")}`:""}function f(e){const t=a(),[n,r]=s.useState(t);return u(()=>{r(i())},[]),typeof e=="string"?e:typeof window>"u"?t:n}export{f as u};
